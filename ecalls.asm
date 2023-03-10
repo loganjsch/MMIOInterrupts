aa5ea79a3470be72fae85d8c5e258eaac726feec
loganjsch <loganjsch@gmail.com>
2023-03-10 10:54:38 -0800

    Update ecalls.asm
    updated ecalls to work with ecalltest.asm

--- ecalls.asm
+++ ecalls.asm
@@ -1,50 +1,33 @@
-.globl printint, printstring, printchar, readchar, readint, exit0, readstring
+.globl printInt, printString, printChar, readChar, readInt, exitProgram, readString
 
-printint:
+printInt:
 		li a7, 1 		# service number to print int
 		ecall			# get user input 
 		ret
-printstring:
+printString:
 		li a7, 4		# sets service number to print string
 		ecall
 		ret
-printchar:
+printChar:
 		li a7, 11		# service number to print char
 		ecall
 		ret
-readchar:
+readChar:
 		li a7, 12 		# load user char y/n
 		ecall
 		ret
 		
-readstring:
-
-	sw ra, svra, t6
-	mv t0, s3
-	li t3, 10
-	li t4, '\0'
-	
-	here:
-		
-		jal readchar
-		beq t3, a0, endhere
-		mv t1, a0
-		sb t1, 0(t0)
-		addi t0, t0, 1
-	
-		b here
-
-	endhere:
-		mv t1, t4
-		sb t1, 0(t0)
-		lw ra, svra
+readString:
+		li a1, 255
+		li a7, 8
+		ecall
 		ret
 			
-readint:
+readInt:
 		li a7, 5 		# service number to read int to a0
 		ecall			# get user input 
 		ret
-exit0:
+exitProgram:
 		li a7, 10		# loads service number for exiting 
 		ecall			# exits program
 		ret
