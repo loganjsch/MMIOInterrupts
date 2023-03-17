701dcfc6c1cf73048a3bf4279726356989663ad8
loganjsch <loganjsch@gmail.com>
2023-03-17 10:27:13 -0700

    isr and test ready
    ready for submit

--- isr.asm
+++ isr.asm
@@ -1,8 +1,10 @@
+# Nichoals Zarate
+# Logan Schwarz
 .globl isr, count
 
 isr:
-	addi sp, sp, -32
-	sw ra, 28(sp)
+	addi sp, sp, -32	# initialize the stack 
+	sw ra, 28(sp)		# store saved values 
 	sw t0, 24(sp)
 	sw t1, 20(sp)
 	sw t2, 16(sp)
@@ -30,7 +32,7 @@ isr:
 	la a0, msg
 	jal printString
 	
-	lw ra, 28(sp)
+	lw ra, 28(sp)	# reload all registers
 	lw t0, 24(sp)
 	lw t1, 20(sp)
 	lw t2, 16(sp)
@@ -38,13 +40,13 @@ isr:
 	lw t4, 8(sp)
 	lw t5, 4(sp)
 	
-	addi sp, sp, 32
+	addi sp, sp, 32	# close stack counter
 	ret
 
 handler:
-	addi sp, sp, -64
-	sw ra, 60(sp)
-	sw a7, 56(sp)
+	addi sp, sp, -64	# intiialzie stack 
+	sw ra, 60(sp)		# save all registers for return from stack 
+	sw a7, 56(sp)	
 	sw s0, 52(sp)
 	sw s1, 48(sp)
 	sw s2, 44(sp)
@@ -57,51 +59,42 @@ handler:
 	sw t5, 16(sp)
 	sw t6, 12(sp)
 	
-	
-	
-	#csrrs s1, uepc, zero
-	#addi s1, s1, 4
-	#csrrw zero, uepc, s1
-	
-	jal readChar
+
+	jal readChar	# read char 
 	mv s0, a0
 	
-	la a0, pressed
-	jal printString
+	la a0, pressed	# jump pressed
+	jal printString	# print tring 
 	
 	mv a0, s0
-	jal printChar
+	jal printChar	# print char 
 	
 	li a0, '\n'
 	jal printChar
 	
-	if:
+	if:			# block for counter
 		li t5, 4
 		la s2, count
-		lw s3, 0(s2)
-		#addi s3, s3, 1
-		#la s2, count
-		#sw s3, 0(s2)
+		lw s3, 0(s2)	# s3 is value of counter
 		
-		blt s3, t5, end_if
+		blt s3, t5, end_if	# check if counter has gone through 5 iterations
 		
-		la t6, main
-		csrrw zero, 65, t6
-		la t5, count
+		la t6, main		# load main address to uret
+		csrrw zero, 65, t6	# hacking uret to return to main 
+		la t5, count		
 		li t6, 0
 		sw t6, 0(t5)
 		uret
 		
 	end_if:
 	
-	addi s3, s3, 1
+	addi s3, s3, 1		# increment counter
 	la s2, count
 	sw s3, 0(s2)
 	
-	mv a0, s0
+	mv a0, s0		
 	
-	
-	lw ra, 60(sp)
+	lw ra, 60(sp)		# reload all the saved registers 
 	lw a7, 56(sp)
 	lw s0, 52(sp)
 	lw s1, 48(sp)
@@ -115,21 +108,14 @@ handler:
 	lw t5, 16(sp)
 	lw t6, 12(sp)
 	addi sp, sp, 64
-	uret
-
-	
-
-
-
+	uret			# return 
 
 .data
 
-msg: .string "\nIn\n"
-pressed: .string "\nkey: "
+msg: .string "\nInitializing Interrupts\n"
+pressed: .string "\nKey Pressed is: "
 RCR: .word 0xffff0000
 DDR:	.word	0xffff000c
 count: .word 0
 
-# "\nInitializing Interrupts\n"
-# "\nkey pressed is: "
 
--- testint.asm
+++ testint.asm
@@ -1,25 +1,14 @@
+# Nicholas Zarate
+# Logan Schwarz
 .text
 .globl main
 
 main: 
-	jal isr
+	jal isr		
 	li a0, '*'
 	
-body:
+body:		
 	mv s1, a0
-	#li s0, 5
-	
-	#la s2, count
-	#lw s3, 0(s2)
 
-	
-	#beq s3, s0, end_body
-	
 	jal printChar
 	b body
-	
-#end_body:
-#	Ela s4, count
-#	li s5, 0
-#	sw s5, 0(s4)
-#	b main
